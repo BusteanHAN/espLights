3e837ceda88c9d574609f65e9148aa1a5e77c965
BusteanHAN <bogdancorbean@gmail.com>
2024-04-13 23:21:28 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <MQTT.h>
+#include <ESP8266WiFi.h>
+#include <FastLED.h>
+
+#include "config.h"
+#include "utils.h"
+
+CRGB leds[NUM_LEDS];
+
+WiFiClient net;
+MQTTClient client;
+
+CRGBPalette16 currentPalette;
+TBlendType currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+uint8_t BRIGHTNESS = 255;
+static uint8_t startIndex = 0;
+bool rgb = false;
+bool on = false;
+String color = "#000000", prevColor = "#000000";
+
+void setup()
+{
+  // Serial.begin(74880);
+  pinMode(1, OUTPUT);
+
+  FastLED.addLeds<LED_TYPE, 1, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  WiFi.begin(SSID, WIFI_PASS);
+
+  client.begin(BROKER_URL, net);
+  client.onMessage(messageReceived);
+  
+  while(!connect()) {;}
+
+  client.subscribe((String)UNIQUE_TOPIC + "/set");
+  client.subscribe((String)GENERAL_TOPIC + "/set");
+
+  currentPalette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
+}
+
+void loop()
+{
+  if ((WiFi.status() != WL_CONNECTED) | (!client.connected())){
+    connect();
+  }
+
+  client.loop();
+  lightLoop();
+  FastLED.show();
+  FastLED.delay(1);
+}
---
+++ src/utils.h
@@ -0,0 +1,169 @@
+#include <Arduino.h>
+#include <MQTT.h>
+#include <ESP8266WiFi.h>
+#include <FastLED.h>
+#include "json.hpp"
+using json = nlohmann::json;
+
+bool connect()
+{
+  // Serial.println("Entered connect()");
+  //set all leds to black
+  uint8_t i;
+  for (i = 0; i < NUM_LEDS; i++)
+    leds[i] = CRGB::Black;
+
+  i = 0;
+
+  FastLED.show();  
+  leds[0] = CRGB(0x00, 0x00, 0x02);
+  FastLED.show();
+
+  // Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    // Serial.print(".");
+    // WiFi.reconnect();
+    delay(1000);
+    i++;
+    if (i > 11){
+      // Serial.println("WiFi connection failed");
+      leds[0] = CRGB(0x02, 0x00, 0x00);
+      FastLED.show();
+      return false;
+    }
+  }
+  leds[0] = CRGB(0x00, 0x02, 0x00);
+  FastLED.show();
+  // Serial.println("Connected to WiFi");
+
+  i = 0;
+
+  leds[1] = CRGB(0x00, 0x00, 0x02);
+  FastLED.show();
+
+  // Serial.print("Connecting to MQTT");
+  while (!client.connect(DEVICE_NAME, "try", "try")) // lamp | shelf
+  {
+    // Serial.print(".");
+    delay(1000);
+    i++;
+    if (i > 11){
+      // Serial.println("MQTT connection failed");
+      leds[1] = CRGB(0x02, 0x00, 0x00);
+      FastLED.show();
+      return false;
+    }
+  }
+  leds[1] = CRGB(0x00, 0x02, 0x00);
+  FastLED.show();
+  client.subscribe((String)UNIQUE_TOPIC + "/set");
+  client.subscribe((String)GENERAL_TOPIC + "/set");
+  // Serial.println("Connected to MQTT");
+  delay(1000);
+
+  FastLED.clear();
+  FastLED.show();  
+  // Serial.println("Cleared Leds");
+  
+  return true;
+}
+
+uint8_t hexConcat(uint8_t a, uint8_t b)
+{
+  return (a << 4) | b;
+}
+
+uint8_t hexStringToUint8(char a)
+{
+  switch (a)
+  {
+  case '0':
+    return 0x0;
+  case '1':
+    return 0x1;
+  case '2':
+    return 0x2;
+  case '3':
+    return 0x3;
+  case '4':
+    return 0x4;
+  case '5':
+    return 0x5;
+  case '6':
+    return 0x6;
+  case '7':
+    return 0x7;
+  case '8':
+    return 0x8;
+  case '9':
+    return 0x9;
+  case 'A':
+  case 'a':
+    return 0xA;
+  case 'B':
+  case 'b':
+    return 0xB;
+  case 'C':
+  case 'c':
+    return 0xC;
+  case 'D':
+  case 'd':
+    return 0xD;
+  case 'E':
+  case 'e':
+    return 0xE;
+  case 'F':
+  case 'f':
+    return 0xF;
+  default:
+    return 0x0;
+  }
+}
+
+void FillLEDsFromPaletteColors(uint8_t colorIndex)
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = ColorFromPalette(currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+    colorIndex += 3;
+  }
+}
+
+void messageReceived(String &topic, String &payload)
+{
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  json in = json::parse(payload);
+  on = !in["state"].is_null() ? in["state"] == "ON" ? true : false : on;
+  prevColor = color;
+  color = !in["color"].is_null() ? String(in["color"].dump().c_str()) : color;
+  rgb = !in["rgbMode"].is_null() ? in["rgbMode"] == "ON" ? true : false : rgb;
+  if (prevColor != color) rgb = false;
+  json out;
+  out["state"] = on ? "ON" : "OFF";
+  out["color"] = color;
+  out["rgbMode"] = rgb ? "ON" : "OFF";
+  client.publish((String)UNIQUE_TOPIC + "/state", out.dump().c_str());
+}
+
+void lightLoop()
+{
+  startIndex != 255 ? startIndex++ : startIndex = 0;
+
+  uint8_t r, g, b;
+  r = hexConcat(hexStringToUint8(color.charAt(2)), hexStringToUint8(color.charAt(3)));
+  g = hexConcat(hexStringToUint8(color.charAt(4)), hexStringToUint8(color.charAt(5)));
+  b = hexConcat(hexStringToUint8(color.charAt(6)), hexStringToUint8(color.charAt(7)));
+
+  if (on) 
+    if (rgb == true) 
+      FillLEDsFromPaletteColors(startIndex);
+    else
+      for (int i = 0; i < NUM_LEDS; i++) leds[i] = CRGB(r, g, b);
+  else 
+    for (int i = 0; i < NUM_LEDS; i++) leds[i] = CRGB::Black;
+
+
+  FastLED.show();
+}
